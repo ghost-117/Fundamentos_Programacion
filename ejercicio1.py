2a3a7ed04637abff688a232440a029f6b7540e15
ghost-117 <krono117pro1@gmail.com>
2024-10-10 09:54:50 -0600

    Ejercicio 01

---
+++ ejercicio1.py
@@ -0,0 +1,8 @@
+'''
+Ejercicio1:Escribir un programa que pregunte al usuario su nombre e imprima un saludo
+Entradas: nombre:String 
+salida: saludo:String
+'''
+nombre= input("Escribe tu nombre:")
+saludo= "Hola " + nombre + "Estas muy Guap@!"
+print(saludo)
