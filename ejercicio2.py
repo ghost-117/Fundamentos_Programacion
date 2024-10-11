f7759f00d8afc4de99098cb20116bef24dc20457
ghost-117 <krono117pro1@gmail.com>
2024-10-11 13:02:21 -0600

    Ejercicio 2

---
+++ ejercicio2.py
@@ -0,0 +1,11 @@
+"""
+
+"""
+base= input("Ingresa la base: ")
+base= int(base)
+altura= input("Altura: ")
+altura= int(altura)
+area =base *altura
+perimetro= (base + altura) * 2
+print (" El area del rectangulo es: " , area)
+print ("El perimetro del rectangulo es: " , perimetro)
