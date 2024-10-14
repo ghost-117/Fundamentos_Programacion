53404056e194db46ca279566327c11ef638ee80f
ghost-117 <krono117pro1@gmail.com>
2024-10-14 09:42:57 -0600

    Ejercico 03

---
+++ Ejercicio3.py
@@ -0,0 +1,12 @@
+"""
+Ejercicio 3:Programa que calcula la hipotenusa de un triangullo rectangulo a partir de sus catetos
+entrada: cateto1: int
+cateto2: int 
+salidas hipotenusa
+"""
+cat1 = input("Escribe el cateto 1:")
+cat2: int(cat1)
+cat2 = int(input("Escribe el cateto 2:"))
+hipo= cat1 * cat1 + cat2 * cat2
+hipo =hipo ** (1/2)
+print("La hipotenusa es: " + hipo)
